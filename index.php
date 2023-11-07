9f0a6ed72b09417ecf226309b6f03ac97e22b625
Andrea Pollastri <a.pollastri@axio.studio>
2023-11-07 18:08:40 +0100

    Wip...

--- index.php
+++ index.php
@@ -17,3 +17,105 @@ $sede = $fattura->createModel(new Sede(
 ));
 
 var_dump($sede);
+
+echo $fattura->createXml(
+    $fattura->modelToArray($sede)
+);
+
+/* 
+$data = [
+    'FatturaElettronicaHeader' => [
+        'DatiTrasmissione' => [
+            'IdTrasmittente' => [
+                'IdPaese' => 'IT',
+                'IdCodice' => '12345678901',
+            ],
+            'ProgressivoInvio' => '2023',
+            'FormatoTrasmissione' => 'FPR12',
+            'CodiceDestinatario' => '0000000',
+        ],
+        'CedentePrestatore' => [
+            'DatiAnagrafici' => [
+                'IdFiscaleIVA' => [
+                    'IdPaese' => 'IT',
+                    'IdCodice' => '12345678901',
+                ],
+                'CodiceFiscale' => '12345678901',
+                'Anagrafica' => [
+                    'Denominazione' => 'Denominazione',
+                ],
+                'RegimeFiscale' => 'RF01'
+            ],
+            'Sede' => [
+                'Indirizzo' => 'Indirizzo',
+                'CAP' => '12345',
+                'Comune' => 'Comune',
+                'Provincia' => 'MI',
+                'Nazione' => 'IT',
+            ]
+        ],
+        'CessionarioCommittente' => [
+            'DatiAnagrafici' => [
+                'IdFiscaleIVA' => [
+                    'IdPaese' => 'IT',
+                    'IdCodice' => '12345678902',
+                ],
+                'CodiceFiscale' => '12345678902',
+                'Anagrafica' => [
+                    'Denominazione' => 'Cliente',
+                ]
+            ],
+            'Sede' => [
+                'Indirizzo' => 'Indirizzo',
+                'CAP' => '12345',
+                'Comune' => 'Comune',
+                'Provincia' => 'MI',
+                'Nazione' => 'IT',
+            ]
+        ]
+    ],
+    'FatturaElettronicaBody' => [
+        'DatiGenerali' => [
+            'DatiGeneraliDocumento' => [
+                'TipoDocumento' => 'TD01',
+                'Divisa' => 'EUR',
+                'Data' => '2021-01-01',
+                'Numero' => '1',
+                'ImportoTotaleDocumento' => '100.00',
+                'Causale' => 'Causale',
+            ]
+        ],
+        'DatiBeniServizi' => [
+            'DettaglioLinee' => [
+                'NumeroLinea' => '1',
+                'Descrizione' => 'Descrizione',
+                'Quantita' => '1.00',
+                'PrezzoUnitario' => '100.00',
+                'PrezzoTotale' => '100.00',
+                'AliquotaIVA' => '22.00',
+            ],
+            'DatiRiepilogo' => [
+                'AliquotaIVA' => '22.00',
+                'ImponibileImporto' => '100.00',
+                'Imposta' => '22.00',
+            ]
+        ],
+        'DatiPagamento' => [
+            'CondizioniPagamento' => 'TP02',
+            'DettaglioPagamento' => [
+                'Beneficiario' => 'Axio Studio S.r.l.',
+                'ModalitaPagamento' => 'MP01',
+                'DataScadenzaPagamento' => '2021-01-01',
+                'ImportoPagamento' => '100.00',
+                'IstitutoFinanziario' => 'Istituto Finanziario',
+                'IBAN' => 'IT95K0842571151000031526155',
+                'ABI' => '08425',
+                'CAB' => '71151',
+                'BIC' => 'CRACIT33',
+                'DataRiferimentoTerminiPagamento' => '2021-01-01',
+                'GiorniTerminiPagamento' => '20',
+            ]
+        ]
+    ]
+];
+*/
--- src/FatturaElettronica.php
+++ src/FatturaElettronica.php
@@ -2,6 +2,7 @@
 
 namespace Axiostudio\FatturaElettronica;
 
+use Spatie\ArrayToXml\ArrayToXml;
 use Symfony\Component\Validator\Validation;
 use Axiostudio\FatturaElettronica\Contracts\Model;
 
@@ -21,5 +22,26 @@ class FatturaElettronica
         }
 
         return $model;
+    }
+
+    public function modelToArray(Model $model): array
+    {
+        return json_decode(json_encode($model), true);
+    }
+
+    public function createXml(array $array): string
+    {
+        $xml = ArrayToXml::convert($array);
+
+        return $this->updateXml($xml);
+    }
+
+    protected function updateXml(string $xml, string $version = 'FPR12'): string
+    {
+        $xml = str_replace('<?xml version="1.0"?>', '<v1:FatturaElettronica xmlns:v1="http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2" versione="' . $version . '">', $xml);
+        $xml = str_replace('<root>', '', $xml);
+        $xml = str_replace('</root>', '</v1:FatturaElettronica>', $xml);
+
+        return $xml;
     }
 }
