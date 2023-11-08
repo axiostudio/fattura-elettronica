3e368c1fa9de530578a0554a736dbfa41cb7758b
Andrea Pollastri <a.pollastri@axio.studio>
2023-11-08 16:13:58 +0100

    Wip...

--- src/Settings.php
+++ src/Settings.php
@@ -4,11 +4,6 @@ namespace Axiostudio\FatturaElettronica;
 
 class Settings
 {
-    public static function RegimeFiscale(): array
-    {
-        return ['RF01', 'RF02', 'RF04', 'RF05', 'RF06', 'RF07', 'RF08', 'RF09', 'RF10', 'RF11', 'FR12', 'RF13', 'RF14', 'RF15', 'RF16', 'RF17', 'RF18', 'RF19'];
-    }
-
     public static function FormatoTrasmissione(): array
     {
         return ['FPA12', 'FPR12'];
@@ -27,6 +22,11 @@ class Settings
     public static function TipoDocumentoDefault(): string
     {
         return 'TD01';
+    }
+
+    public static function RegimeFiscale(): array
+    {
+        return ['RF01', 'RF02', 'RF04', 'RF05', 'RF06', 'RF07', 'RF08', 'RF09', 'RF10', 'RF11', 'FR12', 'RF13', 'RF14', 'RF15', 'RF16', 'RF17', 'RF18', 'RF19'];
     }
 
     public static function CondizioniPagamento(): array
