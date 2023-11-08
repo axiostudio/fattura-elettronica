ea5a766716228760c9a7652049693f0ecd52659b
Andrea Pollastri <a.pollastri@axio.studio>
2023-11-08 12:29:10 +0100

    Fix

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 require('vendor/autoload.php');
 
 use Axiostudio\FatturaElettronica\FatturaElettronica;
-use Axiostudio\FatturaElettronica\Models\CedentePrestatore;
 
 $fattura = new FatturaElettronica();
 
-$sede = $fattura->createModel(new CedentePrestatore(
-    ['12345678910', 'test srl'],
-    ['via roma', '12345', 'roma', 'rm']
-), true);
-
-var_dump($sede);
+$sede = $fattura->header(
+    ['12345678910', '123'],
+    [['12345678910', 'fornitore srl'], ['via roma', '12345', 'roma', 'rm']],
+    [['12345678910', 'cliente srl'], ['via roma', '12345', 'roma', 'rm']]
+);
 
 echo $fattura->createXml($sede);
 
--- src/FatturaElettronica.php
+++ src/FatturaElettronica.php
@@ -6,4 +6,13 @@ class FatturaElettronica
 {
     use \Axiostudio\FatturaElettronica\Handlers\ModelHandlers;
     use \Axiostudio\FatturaElettronica\Handlers\XmlHandlers;
+
+    public function header(array $datiTrasmissione,  array $cedentePrestatore, array $cessionarioCommittente): array
+    {
+        return [
+            'DatiTrasmissione' => $this->createModel(new Models\DatiTrasmissione(...$datiTrasmissione), true),
+            'CedentePrestatore' => $this->createModel(new Models\CedentePrestatore(...$cedentePrestatore), true),
+            'CessionarioCommittente' => $this->createModel(new Models\CessionarioCommittente(...$cessionarioCommittente), true),
+        ];
+    }
 }
