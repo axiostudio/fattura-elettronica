7808cb5f0d00368becce910adb1ede6028685d9f
Andrea Pollastri <a.pollastri@axio.studio>
2023-11-09 12:49:38 +0100

    Wip...

---
+++ src/Models/DettaglioLinee.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Axiostudio\FatturaElettronica\Models;
+
+use Axiostudio\FatturaElettronica\Settings;
+use Axiostudio\FatturaElettronica\Abstracts\Model;
+use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+
+/* 
+$linee[] = new \Deved\FatturaElettronica\FatturaElettronica\FatturaElettronicaBody\DatiBeniServizi\Linea(
+    $linea['descrizione'],
+    $linea['prezzoUnitario'],
+    $codiceArticolo,
+    $quantita,
+    $unitaMisura,
+    $aliquotaIva,
+    $codiceTipo,
+    request()->omnifyCompany['decimali'],
+    $tipoCessionePrestazione,
+    $natura,
+);
+*/
+
+class DettaglioLinee extends Model
+{
+    public string $Descrizione;
+    public float $PrezzoUnitario;
+    public ?int $Quantita;
+    public ?string $UnitaMisura;
+    public ?float $AliquotaIVA;
+    public ?string $Natura;
+    public ?string $CodiceArticolo;
+    public ?string $CodiceTipo;
+
+    public function __construct(...$args)
+    {
+        $this->Descrizione = $args[0];
+        $this->PrezzoUnitario = $args[1];
+        $this->Quantita = (isset($args[2]) && $args[2]) ? $args[2] : Settings::QuantitaDefault();
+        $this->UnitaMisura = (isset($args[3]) && $args[3]) ? $args[3] : Settings::UnitaMisuraDefault();
+        $this->AliquotaIVA = (isset($args[4]) && $args[4]) ? $args[4] : Settings::AliquotaIVADefault();
+
+        if (isset($args[5]) && $args[5]) {
+            $this->Natura = $args[5];
+        }
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addPropertyConstraint('UnitaMisura', new Choice(Settings::UnitaMisura()));
+        $metadata->addPropertyConstraint('Natura', new Choice(Settings::Natura()));
+    }
+}
--- src/Settings.php
+++ src/Settings.php
@@ -29,6 +29,11 @@ class Settings
         return ['RF01', 'RF02', 'RF04', 'RF05', 'RF06', 'RF07', 'RF08', 'RF09', 'RF10', 'RF11', 'FR12', 'RF13', 'RF14', 'RF15', 'RF16', 'RF17', 'RF18', 'RF19'];
     }
 
+    public static function Natura(): array
+    {
+        return ['N1', 'N2', 'N2.1', 'N2.2', 'N3', 'N3.1', 'N3.2', 'N3.3', 'N3.4', 'N3.5', 'N3.6', 'N4', 'N5', 'N6', 'N6.1', 'N6.2', 'N6.3', 'N6.4', 'N6.5', 'N6.6', 'N6.7', 'N6.8', 'N6.9', 'N7'];
+    }
+
     public static function CondizioniPagamento(): array
     {
         return ['TP01', 'TP02', 'TP03'];
@@ -62,6 +67,26 @@ class Settings
     public static function IdPaeseDefault(): string
     {
         return 'IT';
+    }
+
+    public static function QuantitaDefault(): int
+    {
+        return 1;
+    }
+
+    public static function UnitaMisura(): array
+    {
+        return ['PZ', 'pz', 'NR', 'N', 'N.', 'n.', 'EA', 'CAD', 'LT', 'ST', 'PCE', 'RT', 'KG', 'UM', 'UL', 'giorni', 'mc', 'UTA'];
+    }
+
+    public static function UnitaMisuraDefault(): string
+    {
+        return 'pz';
+    }
+
+    public static function AliquotaIVADefault(): float
+    {
+        return 22.00;
     }
 
     public static function CodiceDestinatarioDefault(): string
